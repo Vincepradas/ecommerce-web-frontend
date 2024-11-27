e9aed6f874081a03b49aa1168c9dc749c17750bc
Vince Pradas <pradasvince@gmail.com>
2024-11-27 11:47:04 +0800

    fixing blank page upon build t1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom"; // No BrowserRouter or HashRouter here
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
@@ -9,19 +9,19 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </Router>
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products/:id" element={<ProductDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 import "./style/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// Wrap the entire app with HashRouter at the root level
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <HashRouter>
+            <App />
+        </HashRouter>
     </React.StrictMode>
 );
